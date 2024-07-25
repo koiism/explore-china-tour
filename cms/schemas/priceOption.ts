d1fff1ffdf30eb37a3ea578c2e3d56c8c7fca226
CodingMie <544286175@qq.com>
2024-07-25 17:18:36 +0800

    add name

--- cms/schemas/priceOption.ts
+++ cms/schemas/priceOption.ts
@@ -5,11 +5,7 @@ export default defineField({
   title : 'Price Option',
   type : 'document',
   fields:[
-<<<<<<< HEAD
     defineField({ name: 'name', title: 'name', type: 'string', }),
-=======
-    defineField({ name: 'title', title: 'Title', type: 'string', }),
->>>>>>> 18ba5bfbc7a801efed4378ff01e47a9e87751519
     defineField({ name: 'price', title: 'Price(USD)', type: 'number', }),
     defineField({ name: 'ticketApplicability', title: 'Ticket Applicability', type: 'string', }),
   ]
